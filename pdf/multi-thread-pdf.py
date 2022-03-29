e654ad1cb0e8b481be8f0700c3123cffc2a61d3a
henryhust <1277915903@qq.com>
2022-03-29 09:35:38 +0800

    feat: pdf

--- pdf/multi-thread-pdf.py
+++ pdf/multi-thread-pdf.py
@@ -1,9 +1,31 @@
-import threading
-import csv
+import os
+from typing import *
 import pdfkit
+import logging
+import threading
 
-read_file_path = "./kbs/data/csv/kbs.csv"
-write_file_path = "./kbs/data/pdf/%s.pdf"
+output_path = "./data/npr"
+
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
+
+def mongo_datas(media_id: int):
+    import pymongo
+
+    MONGO_URI = 'mongodb://root:aini1314@39.107.26.235:27017'
+    myclient = pymongo.MongoClient(MONGO_URI)
+
+    mydb = myclient['media']        # 选择media数据库
+    mycollection = mydb["news"]     # 选择news数据表
+
+    query = {"media_id": media_id}  # 查询条件
+    res = mycollection.find(query)
+
+    result = [(item["news_url"], item["news_pdf"]) for item in res]
+    myclient.close()
+    print("mongo数据获取完成， 共{}条".format(str(len(result))))
+    return result
 
 
 def download_pdf(url, write_filepath=None):
@@ -17,23 +39,25 @@ def download_pdf(url, write_filepath=None):
         'encoding': "UTF-8",
     }
     try:
-        pdfkit.from_url(url, write_file_path % url.split("?")[-1], options=options)
-        # pdfkit.from_url(url, write_filepath, options=options)
+        pdfkit.from_url(url, write_filepath, options=options)
+        print("success!")
     except:
+        print("wrong")
         pass
 
 
 def thread_func(tasks):  # 线程函数
-    for url in tasks:
-        download_pdf(url)
+    for (url, pdf_filename) in tasks:
+        filepath = os.path.join(output_path, pdf_filename)
+        if os.path.isfile(filepath):
+            continue
+        download_pdf(url, write_filepath=filepath)
 
 
 def multi_thread_run(thread_num):
-    news_detail_url_list = []
-    csv_reader = csv.reader(open(read_file_path))
-    for line in csv_reader:
-        news_detail_url_list.append(line[9])
-    news_detail_url_list = news_detail_url_list[1:]
+
+    news_detail_url_list = mongo_datas(media_id=24)
+
     n = thread_num
     num = len(news_detail_url_list) // n
 
