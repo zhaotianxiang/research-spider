5241c3abfed95638d37750f8b9d0c725d64e96d7
zhaotianxiang <904001993@qq.com>
2022-04-16 11:38:20 +0800

    feat: 共同通讯社完美

--- media/spiders/kyodo.py
+++ media/spiders/kyodo.py
@@ -6,14 +6,15 @@ import sys
 from scrapy.linkextractors import LinkExtractor
 from urllib.parse import urlparse
 
-from ..items import ReporterItem
 from ..items import NewsItem
+from ..items import ReporterItem
 
 
+# 共同通讯社
 class Spider(scrapy.Spider):
     id = 22
     name = 'kyodo'
-    allowed_domains = ['english.kyodonews.net', 'china.kyodonews.net', 'www.47news.jp'][0:2]
+    allowed_domains = ['english.kyodonews.net', 'china.kyodonews.net', 'www.47news.jp']
     start_urls = ['https://china.kyodonews.net/']
 
     def parse(self, response):
@@ -31,46 +32,34 @@ class Spider(scrapy.Spider):
                 yield scrapy.Request(url)
 
     def news(self, response):
-        # response_json = None
-        # if re.search('47news', response.url):
-        #     print(re.findall(r'(?<=ld\+json">).*?(?=</script>)', response.text))
-        #     self.logger.warn("================= %s", response.url)
-        #     return None
-        # else:
-        response_json = json.loads(re.findall(r'(?<=ld\+json">)\[.*?\](?=</script)', response.text)[0])[0]
+        if re.search('www.47news.jp', response.url):
+            response_json = json.loads(response.css("script[type=application\/ld\+json]::text").extract_first().strip())
+        else:
+            response_json = \
+                json.loads(response.css("script[type=application\/ld\+json]::text").extract_first().strip())[0]
 
         newsItem = NewsItem()
         newsItem['news_id'] = response.url.split('/')[-1].split('.html')[0]
-        newsItem['news_title'] = response_json['headline']
-        newsItem['news_content'] = "".join(response.css("div.article-body p::text").extract())
-        if re.search("china.kyodonews.net", response.url):
-            newsItem['news_title_cn'] = newsItem['news_title']
-            newsItem['news_content_cn'] = newsItem['news_content']
-        publish_time = response_json["datePublished"]
-        newsItem['news_publish_time'] = datetime.datetime.strptime(publish_time, "%Y-%m-%dT%H:%M:%S%z").strftime('%Y-%m-%d %H:%M:%S')
+        newsItem['news_title'] = response.css("meta[property=og\:title]::attr(content)").extract_first().strip()
+        newsItem['news_keywords'] = response.css("meta[name=keywords]::attr(content)").extract_first().strip()
+        newsItem['news_content'] = response.css("meta[property=og\:description]::attr(content)").extract_first().strip()
+        if 'datePublished' in response_json:
+            newsItem['news_publish_time'] = response_json["datePublished"]
+        else:
+            newsItem['news_publish_time'] = response.css("div.writer > span::text").extract_first()
         newsItem['news_url'] = response.url
         newsItem['news_pdf'] = f"{self.name}_{newsItem['news_id']}.pdf"
         newsItem['news_pdf_cn'] = f"{self.name}_{newsItem['news_id']}_cn.pdf"
         newsItem['reporter_list'] = []
-        reporter_name = response_json["author"]["name"].strip()
-        reporter_id = reporter_name
-        reporterItem = ReporterItem()
-        reporterItem['reporter_id'] = reporter_id
-        reporterItem['reporter_name'] = reporter_name
-        newsItem['reporter_list'].append(reporterItem)
-        yield reporterItem
-        self.logger.warn("保存新闻和记者信息 %s", response.url)
-        if len(newsItem['reporter_list']) == 0:
-            yield None
-        yield newsItem
+        if re.search("china.kyodonews.net", response.url):
+            newsItem['news_title_cn'] = newsItem['news_title']
+            newsItem['news_content_cn'] = newsItem['news_content']
 
-        # 泛查询
-        for link in LinkExtractor(
-                restrict_css='body',
-                allow_domains=self.allowed_domains,
-                canonicalize=True).extract_links(response):
-            url = link.url
-            if re.search('net/news/.*?.html$', url) or re.search('\d{7}.html$', url):
-                yield scrapy.Request(url, callback=self.news)
-            else:
-                yield scrapy.Request(url)
+        if 'name' in response_json["author"]:
+            reporter_name = response_json["author"]["name"].strip()
+            reporterItem = ReporterItem()
+            reporterItem['reporter_id'] = "-".join(reporter_name.split(" "))
+            reporterItem['reporter_name'] = reporter_name
+            newsItem['reporter_list'].append(reporterItem)
+            yield reporterItem
+        yield newsItem
