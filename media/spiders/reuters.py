2ed3279df8708011d70dd0886c5a2f5dd0b328e6
zhaotianxiang <904001993@qq.com>
2022-04-16 19:45:19 +0800

    feat: reuters

--- media/spiders/reuters.py
+++ media/spiders/reuters.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import re
 import scrapy
@@ -5,66 +6,87 @@ import sys
 from scrapy.linkextractors import LinkExtractor
 from urllib.parse import urlparse
 
-from ..items import ReporterItem
 from ..items import NewsItem
+from ..items import ReporterItem
 
-# 定义下载新闻分类的种子
-def seed():
-    return [
-        'https://cn.reuters.com/news/archive/topic-cn-top-news?view=page&page=1&pageSize=10',
-    ]
+
+def get_content(content):
+    if content.get("content"):
+        return content["content"]
+    return ""
 
 
 class Spider(scrapy.Spider):
     id = 6
-    name = 'reuters_cn'
-    start_urls = seed()
+    name = 'reuters'
+    allow_domains = ['www.reuters.com', 'cn.reuters.com', ]
+    start_urls = ['https://www.reuters.com/']
 
     def parse(self, response):
-        news_links = LinkExtractor(restrict_css='#blogStyleNews > section > div > article').extract_links(response)
-        for link in news_links:
-            yield scrapy.Request(link.url, callback=self.parse_news)
-        self.logger.info("URL %s 共 %s 条新闻", response.url, len(news_links))
-        next_links = LinkExtractor(restrict_css='#content a.control-nav-next').extract_links(response)
-        if next_links and len(next_links) == 1:
-            for next_link in next_links:
-                yield scrapy.Request(next_link.url)
+        links = LinkExtractor(restrict_css='body').extract_links(response)
+        self.logger.warn("泛查询 %s --- %s 个子页面", response.url, len(links))
 
-    def parse_news(self, response):
+        for link in LinkExtractor(
+                restrict_css='body',
+                allow_domains=self.allow_domains).extract_links(response):
+            if re.search('reuters.com/.*?\d{2,4}-\d{2}-\d{2}/', link.url):
+                yield scrapy.Request(link.url, callback=self.news)
+            else:
+                yield scrapy.Request(link.url)
+
+    def news(self, response):
         response_json = json.loads(response.css("script[type=application\/ld\+json]::text").extract_first().strip())
+        global_content = json.loads(re.findall(r'(?<=globalContent=){.*?}(?=;)', response.text)[0])
         newsItem = NewsItem()
-        newsItem['news_id'] = response.url.split('/')[-1].split('-')[-1].replace('^id', '')
-        newsItem['news_title'] = response.css('h1.Headline-headline-2FXIq::text').extract_first()
-        newsItem['news_title_cn'] = newsItem['news_title']
-        newsItem['news_content'] = " ".join(response.css('div.ArticleBodyWrapper > p::text').extract())
-        author_describe = response.css(
-            'div.ArticleBodyWrapper div.Attribution-attribution-Y5JpY > p::text').extract_first()
-        newsItem['news_content'] += "\n" + author_describe
-        newsItem['news_content_cn'] = newsItem['news_content']
-        newsItem['news_publish_time'] = datetime.datetime \
-            .strptime(response_json["datePublished"], "%Y-%m-%dT%H:%M:%S%z") \
-            .strftime('%Y-%m-%d %H:%M:%S')
+        newsItem['news_id'] = response.url.split('/')[-2]
+        newsItem['news_title'] = response_json['headline']
+        newsItem['news_title_cn'] = None
+        newsItem['news_content'] = "\n".join(list(map(get_content, global_content["result"]["content_elements"])))
+        newsItem['news_content_cn'] = None
+        newsItem['news_publish_time'] = datetime.datetime.strptime(response_json["datePublished"][0:19],
+                                                                   "%Y-%m-%dT%H:%M:%S").strftime('%Y-%m-%d %H:%M:%S')
         newsItem['news_url'] = response.url
         newsItem['news_pdf'] = f"{self.name}_{newsItem['news_id']}.pdf"
         newsItem['news_pdf_cn'] = f"{self.name}_{newsItem['news_id']}_cn.pdf"
         newsItem['reporter_list'] = []
-        newsItem['media_id'] = self.id
-        newsItem['media_name'] = 'reuters'
 
-        if author_describe:
-            author_list = author_describe.split('；')
-            if author_list and len(author_list):
-                for authors in author_list:
-                    for author in authors.split(" ")[1].split('/'):
-                        reporterItem = ReporterItem()
-                        reporterItem['reporter_id'] = author
-                        reporterItem['reporter_name'] = author
-                        reporterItem['reporter_image'] = None
-                        reporterItem['reporter_image_url'] = None
-                        reporterItem['reporter_intro'] = authors.split(" ")[0]
-                        reporterItem['reporter_url'] = None
-                        reporterItem['media_id'] = self.id
-                        reporterItem['media_name'] = 'reuters'
-                        newsItem['reporter_list'].append(reporterItem)
-                        yield reporterItem
+        for author in global_content["result"]["authors"]:
+            reporterItem = ReporterItem()
+            if author.get('id'):
+                reporterItem['reporter_id'] = author['id']
+            else:
+                reporterItem['reporter_id'] = author['name']
+            reporterItem['reporter_name'] = author['name']
+            reporterItem['reporter_code_list'] = []
+            if author.get('social_links'):
+                for social in author["social_links"]:
+                    reporterItem['reporter_code_list'].append({
+                        "code_type": social["site"],
+                        "code_content": social["url"].replace('mailto:', '')
+                    })
+            reporterItem['reporter_intro'] = author.get('description')
+            reporterItem['reporter_url'] = response.urljoin(author["topic_url"])
+            reporterItem['media_id'] = self.id
+            reporterItem['media_name'] = 'reuters'
+            newsItem['reporter_list'].append(reporterItem)
+            # 进入作者详情页面
+            yield scrapy.Request(url=reporterItem['reporter_url'],
+                                 meta={"reporterItem": reporterItem, "news_url": newsItem['news_url']},
+                                 callback=self.reporter)
         yield newsItem
+
+    def reporter(self, response):
+        # 能补充获取头像 作者简介
+        reporterItem = response.meta["reporterItem"]
+        response_json = None
+        try:
+            response_json = json.loads(re.findall(r'(?<=globalContent=){.*?}(?=;)', response.text)[0])
+        except:
+            pass
+        if response_json:
+            author = response_json["result"]["topics"][0]
+            reporterItem["reporter_image_url"] = author['thumbnail']['url']
+            reporterItem['reporter_image'] = f"reuters_{reporterItem['reporter_id']}.jpg"
+            if author.get('description'):
+                reporterItem["reporter_intro"] = author['description']
+        yield reporterItem
