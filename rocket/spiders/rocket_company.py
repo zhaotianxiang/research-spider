c7323ffef6b6b6aea6c8d84a6f499f634bca9dd0
zhaotianxiang <904001993@qq.com>
2022-06-14 22:32:05 +0800

    for researcher

--- rocket/spiders/rocket_company.py
+++ rocket/spiders/rocket_company.py
@@ -1,58 +1,92 @@
+# 程序目的： 根据公司名称获取所有员工信息
+#
+# 1. 修改 company_list 里面的值， 多个公司增加多个值：
+#    如：   company_list = [
+#            '"Bryant Christie Inc."',
+#            '"KBS"',
+#        ]
+#
+# 2. 确认项目根路径下 scrapy.cfg 文件内容为：
+#    [settings]
+#    default = rocket.settings
+#
+# 3. 进入 C:\Users\spider7\Downloads\research-spider-master 文件夹， 选中上方的文件夹地址栏， 输入 cmd 后回车
+#
+# 4. 在黑框中运行程序输入： python -m scrapy crawl rocket_company
+#
+# 5. 结果见： C:\Users\spider7\Downloads\research-spider-master\data\csv 文件夹
+#    人员头像见： C:\Users\spider7\Downloads\research-spider-master\data\image 文件夹
+
 import json
 import logging
 import re
-
+import rocketreach
 import scrapy
+import logging
+import pymongo
+import rocketreach
+import scrapy
+import json
 
 logger = logging.getLogger(__name__)
+
+########################################################
+# 运行程序前确认需要搜索的公司名称如下，双引号表示精确搜索：
+########################################################
+company_list = [
+    '"Bryant Christie Inc."'
+]
+
+
+def parse_job_history(item):
+    ret = ""
+    if not item.get("start_date"):
+        ret += "None" + "～"
+    else:
+        ret += item["start_date"] + "～"
+    if not item.get("end_date"):
+        ret += "None"
+    else:
+        ret += item["end_date"]
+    ret += ":" + item["company_name"]
+    return ret
+
+
+def parse_education(item):
+    ret = ""
+    if not item.get("start"):
+        ret += "None" + "～"
+    else:
+        ret += str(item["start"]) + "～"
+    if not item.get("end"):
+        ret += "None"
+    else:
+        ret += str(item["end"])
+    school = " "
+    if 'school' in item:
+        school = item['school']
+    ret += ":" + str(school)
+    return ret
+
+
+logger = logging.getLogger(__name__)
+
 
 class Spider(scrapy.Spider):
     name = 'rocket_company'
     allowed_domains = []
     key = 'ab52edkd117f699da82a53926b1db64f8a215ed'
-    # test account
-    # key = 'a4f232k9333d66b17f359eec8b1e4b89de31df6'
 
     def start_requests(self):
-        company_list = [
-            '"KBS (Korean Broadcasting System)"',
-            '"Voice of America"',
-            '"Thomson Reuters"',
-            '"Associated Press"',
-            'Youmiuri',
-            '"Asahi Shinbun"',
-            '"Financial Times"',
-            '"United Press International"',
-            '"Associated Press"',
-            '"Bloomberg Lp"',
-            '"Nhk, Japan Broadcasting Corporation"',
-            '"NHK World Japan"',
-            '"The Washington Post"',
-            '"WSJ Magazine"',
-            '"WSJ the Wall Street Journal Magazine"',
-            '"The Wall Street Journal"'
-            '"Nbcuniversal Media, LLC"',
-            '"The Times"',
-            '"Time Magazine"',
-            '"NTD Television"',
-            '"CNN"',
-            '"The Globe and Mail"',
-            '"Mirror"',
-            '"Epoch Times"',
-            '"Kyodo News"',
-            '"Chinaaid Association"',
-            '"NPR"'
-        ]
         for company in company_list:
             url = f"https://api.rocketreach.co/v2/api/search"
             data = {
                 "company": company,
-                "query":{"current_employer":[company]},
+                "query": {"current_employer": [company]},
                 "start": 1,
                 "page_size": 100
             }
             yield scrapy.Request(url, body=json.dumps(data), method="POST", meta=data)
-
 
     def parse(self, response):
         company = response.meta["company"]
@@ -63,35 +97,62 @@ class Spider(scrapy.Spider):
         peoples = result['profiles']
         pagination = result['pagination']
 
-        self.logger.info("URL: %-20s Company: %-50s PAGE: %-3s/%-3s GOT: %-3s people", response.url,  company, int(pagination['start']/page_size)+1,
-            int(pagination['total']/page_size), len(peoples))
+        self.logger.info("URL: %-20s Company: %-50s PAGE: %-3s/%-3s GOT: %-3s people", response.url, company,
+                         int(pagination['start'] / page_size) + 1,
+                         int(pagination['total'] / page_size) + 1, len(peoples))
 
         for people in peoples:
-            # judgement this people is reporter
-            current_title = people['current_title']
-            if current_title:
-                reporter_keywords = re.compile(r'news|writer|reporter|publisher|journalist|photographer|cameraman|correspondent|corresponsal|analyst|producer|Television|Commentator|기자', re.I)
-                if reporter_keywords.search(current_title):
-                    # self.logger.info("Company: %-40s Title: %-80s PeopleName: %-50s IS Reporter !!", people['current_employer'], people['current_title'], people['name'])
-                    url = f"https://api.rocketreach.co/v2/api/lookupProfile?api_key={self.key}&id={people['id']}"
-                    yield people
-                    # yield scrapy.Request(url, meta={'people': people}, callback=self.profile)
-                else:
-                    pass
-                    # self.logger.info("Company: %-40s Title: %-80s PeopleName: %-50s NOT Reporter ", people['current_employer'], people['current_title'], people['name'])
-
+            url = f"https://api.rocketreach.co/v2/api/lookupProfile?api_key={self.key}&id={people['id']}"
+            # yield people
+            # 花费会员点数开启此项，关闭上一项
+            yield scrapy.Request(url, meta={'people': people}, callback=self.profile)
         if len(peoples) > 0:
             url = f"https://api.rocketreach.co/v2/api/search"
             data = {
                 "company": company,
-                "query":{"current_employer":[company]},
-                "start": start+page_size,
+                "query": {"current_employer": [company]},
+                "start": start + page_size,
                 "page_size": page_size
             }
-            yield scrapy.Request(url, body=json.dumps(data),meta=data, method="POST")
+            yield scrapy.Request(url, body=json.dumps(data), meta=data, method="POST")
 
     def profile(self, response):
         people = response.meta["people"]
         profile = json.loads(response.text)
         people.update(profile)
-        yield people
+        self.logger.info("cost one credit")
+        item = {
+            '人员编号': people['id'],
+            '人员名称': people['name'],
+            '地区': people['region'],
+            '城市': people['city'],
+            '城市代码': people['country_code'],
+            '雇主信息': people['current_employer'],
+            '当前职位': people['current_title'],
+            '当前领英地址': people['linkedin_url'],
+            '地理位置': people['location'],
+            '正式职位': people['normalized_title'],
+            '人员状态': people['status'],
+            '附加信息': people['teaser'],
+            '头像信息（源数据）': profile['profile_pic'],
+            '社交账号（源数据）': profile['links'],
+            '工作经历（源数据）': profile['job_history'],
+            '教育经历（源数据）': profile['education'],
+            '邮箱地址（源数据）': profile['emails'],
+            '电话号码（源数据）': profile['phones']
+        }
+        if profile['emails'] is not None:
+            item['邮箱列表'] = "\n".join(list(map(lambda e: e['email'], profile['emails'])))
+        if profile['phones'] is not None:
+            item['电话列表'] = "\n".join(list(map(lambda p: p['number'], profile['phones'])))
+        if profile['job_history'] is not None:
+            item['工作列表'] = "\n".join(list(map(parse_job_history, profile['job_history'])))
+        if profile['education'] is not None:
+            item['教育列表'] = "\n".join(list(map(parse_education, profile['education'])))
+        if profile['links'] is not None:
+            for link in profile['links']:
+                item[link] = profile['links'][link]
+        item['最原始详情数据'] = people
+        item['profile_pic'] = people['profile_pic']
+        item['name'] = people['name']
+        yield item
